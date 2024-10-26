9961fac0fae059427d7b29bc653d86586b7f5bc4
Md Rijwan Jannat <rijwanjannat36@gmail.com>
2024-10-26 19:40:04 +0600

    add home button on dashboard

--- config/site.ts
+++ config/site.ts
@@ -5,84 +5,90 @@ import {
   FaGraduationCap,
   FaProjectDiagram,
   FaBlog,
-} from "react-icons/fa";
+  FaHome,
+} from 'react-icons/fa';
 
 export const siteConfig = {
   name: "Md-Rijwan-Jannat's Portfolio",
-  ogImage: "https://demo.jpg",
-  url: "www.rijwanjanant.dev",
+  ogImage: 'https://demo.jpg',
+  url: 'www.rijwanjanant.dev',
   description:
-    "Junior Full Stack Developer specializing in modern web technologies.",
+    'Junior Full Stack Developer specializing in modern web technologies.',
   navItems: [
     {
-      label: "Home",
-      href: "#home",
+      label: 'Home',
+      href: '#home',
     },
     {
-      label: "Skills",
-      href: "#skills",
+      label: 'Skills',
+      href: '#skills',
     },
     {
-      label: "Experience",
-      href: "#experience",
+      label: 'Experience',
+      href: '#experience',
     },
     {
-      label: "About",
-      href: "#about",
+      label: 'About',
+      href: '#about',
     },
     {
-      label: "Education",
-      href: "#education",
+      label: 'Education',
+      href: '#education',
     },
     {
-      label: "Projects",
-      href: "#projects",
+      label: 'Projects',
+      href: '#projects',
     },
     {
-      label: "Blogs",
-      href: "#blogs",
+      label: 'Blogs',
+      href: '#blogs',
     },
     {
-      label: "Contact",
-      href: "#contact",
+      label: 'Contact',
+      href: '#contact',
     },
   ],
 
   dashboardMenuItems: [
-    { name: "Profile", icon: FaUser, path: "/dashboard" },
+    { name: 'Profile', icon: FaUser, path: '/dashboard' },
     {
-      name: "Skills Management",
+      name: 'Skills Management',
       icon: FaLaptopCode,
-      path: "/dashboard/skills-management",
+      path: '/dashboard/skills-management',
     },
     {
-      name: "Projects Management",
+      name: 'Projects Management',
       icon: FaProjectDiagram,
-      path: "/dashboard/projects-management",
+      path: '/dashboard/projects-management',
     },
     {
-      name: "Education Management",
+      name: 'Education Management',
       icon: FaGraduationCap,
-      path: "/dashboard/education-management",
+      path: '/dashboard/education-management',
     },
     {
-      name: "Experience Management",
+      name: 'Experience Management',
       icon: FaUser,
-      path: "/dashboard/experience-management",
+      path: '/dashboard/experience-management',
     },
     {
-      name: "Blogs Management",
+      name: 'Blogs Management',
       icon: FaBlog,
-      path: "/dashboard/blogs-management",
+      path: '/dashboard/blogs-management',
+    },
+    {
+      name: 'Home',
+      icon: FaHome,
+      path: '/',
     },
   ],
   links: {
-    github: "https://github.com/Md-Rijwan-Jannat",
-    linkedin: "https://www.linkedin.com/in/md-rijwan-jannat47",
-    facebook: "https://www.facebook.com/mdrijwanjannat",
-    twitter: "https://x.com/JannatRijw51454",
-    discord: "https://discord.com/channels/r.j.rijwan",
+    github: 'https://github.com/Md-Rijwan-Jannat',
+    linkedin: 'https://www.linkedin.com/in/md-rijwan-jannat47',
+    facebook: 'https://www.facebook.com/mdrijwanjannat',
+    twitter: 'https://x.com/JannatRijw51454',
+    discord: 'https://discord.com/channels/r.j.rijwan',
     resume:
-      "https://drive.google.com/file/d/1YIFQMCuGD8NCdpW4XuSH_01Ft9bPh_2V/view?usp=drive_link",
+      'https://drive.google.com/file/d/1YIFQMCuGD8NCdpW4XuSH_01Ft9bPh_2V/view?usp=drive_link',
   },
 };
